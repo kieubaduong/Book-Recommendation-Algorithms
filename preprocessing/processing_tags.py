6446477e2080e2459360ec3cefcb49cc2f3c0a5c
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-06-03 16:20:22 +0700

    done processing tag

---
+++ preprocessing/processing_tags.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+df = pd.read_csv('../dataset/crawled_dataset/books.csv', encoding='utf-8')
+
+stop_words = set(nltk.corpus.stopwords.words('english'))
+
+def process_tags(row):
+    tags = row['tags']
+    tag_list = tags.split(';')
+    processed_tags = [re.sub(r'[^a-zA-Z]', '', word.strip()) for word in tag_list if re.sub(r'[^a-zA-Z]', '', word.strip()).lower() not in stop_words]
+    processed_tags = ';'.join(processed_tags)
+    return processed_tags
+
+df['tags'] = df.apply(process_tags, axis=1)
+
+df.to_csv('../dataset/processed_dataset/books.csv', index=False)
