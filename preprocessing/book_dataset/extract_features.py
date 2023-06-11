afbc8b3c34b4eeff9ed5e489cf2f9b2cf0a5cd2a
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-06-11 17:06:59 +0700

    extract book feature and add test

---
+++ preprocessing/book_dataset/extract_features.py
@@ -0,0 +1,59 @@
+import numpy as np
+import csv
+import pandas as pd
+from transformers import BertTokenizer, BertModel
+
+df_rating = pd.read_csv('../../dataset/processed_dataset/ratings.csv')
+df_book = pd.read_csv('../../dataset/processed_dataset/books.csv')
+df_user = pd.read_csv('../../dataset/processed_dataset/users.csv')
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+default_features = np.zeros((768,))
+
+def extract_features(book_info):
+    try:
+      title = book_info['title']
+      author = book_info['author']
+      publish_year = book_info['year']
+      publisher = book_info['publisher']
+      tags = book_info['tags']
+      description = book_info['description']
+      genres = book_info['genres']
+
+      max_length = 512 - len(title) - len(author) - 4 - len(publisher)
+      tags = tags[:max_length]
+      description = description[:max_length]
+      genres = genres[:max_length]
+
+      input_text = f"{title} {author} {publish_year} {publisher} {tags} {description} {genres}"
+      inputs = tokenizer.encode_plus(input_text, add_special_tokens=True, padding='max_length', max_length=512, return_tensors='pt')
+      
+      outputs = model(**inputs)
+      hidden_state = outputs.last_hidden_state[:, 0, :].squeeze().detach().numpy()
+      return hidden_state
+    except:
+        return None
+
+error_isbns = []
+
+df_book_features = pd.read_csv("../../dataset/book_features.csv")
+extracted_isbns = df_book_features['isbn'].tolist()
+
+for _, book_info in df_book.iterrows():
+    isbn = book_info['isbn']
+    if isbn in extracted_isbns:
+        continue
+    else:
+        features = extract_features(book_info)
+        if features is not None:
+            with open("../../dataset/book_features.csv", "a", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([isbn, features])
+        else:
+            error_isbns.append(isbn)
+
+
+
+error_isbn_df = pd.DataFrame({'isbn': error_isbns})
+error_isbn_df.to_csv("../../dataset/error_isbns.csv", index=False)
---
+++ test/book_without_rating.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+
+df_rating = pd.read_csv('../dataset/processed_dataset/ratings.csv')
+df_book = pd.read_csv('../dataset/processed_dataset/books.csv')
+df_user = pd.read_csv('../dataset/processed_dataset/users.csv')
+
+
+
+# Kết hợp DataFrame df_book và df_rating dựa trên cột 'isbn'
+merged_df = df_book.merge(df_rating, on='isbn', how='left')
+
+# Lọc các dòng trong df_book không có tương ứng trong df_rating
+books_without_rating = merged_df[merged_df['user-id'].isnull()]
+
+# Tạo một mảng boolean cho biết các quyển sách có trong books_without_rating hay không
+is_not_rated = df_book['isbn'].isin(books_without_rating['isbn'])
+
+# Lọc và loại bỏ các quyển sách không được rating từ DataFrame df_book
+df_book_rated = df_book[~is_not_rated]
+
+# Hiển thị DataFrame df_book sau khi loại bỏ các quyển sách không được rating
+df_book_rated.to_csv('../dataset/processed_dataset/books.csv', index=False)
